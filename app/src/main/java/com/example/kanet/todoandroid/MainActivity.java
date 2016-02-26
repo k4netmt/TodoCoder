d2847c1adb9c05b768c26fbd3c6dadf5fb163247
k4netmt <k4netmt@gmail.com>
2016-02-26 11:01:27 +0700

    To do app

--- app/src/main/java/com/example/kanet/todoandroid/MainActivity.java
+++ app/src/main/java/com/example/kanet/todoandroid/MainActivity.java
@@ -2,12 +2,75 @@ package com.example.kanet.todoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aToDoAdapter;
+    ListView lvItems;
+    EditText etEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populaterArrayItems();
+        lvItems=(ListView)findViewById(R.id.lvItems);
+        lvItems.setAdapter(aToDoAdapter);
+        etEditText=(EditText)findViewById(R.id.etAddNew);
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                todoItems.remove(position);
+                aToDoAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+    }
+
+    public void populaterArrayItems(){
+        todoItems=new ArrayList<String>();
+        readItems();
+        /*todoItems.add("Items 1");
+        todoItems.add("Items 2");
+        todoItems.add("Items 3");*/
+        aToDoAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,todoItems);
+    }
+
+    public void OnAddItems(View view) {
+        aToDoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
+        writeItems();
+    }
+
+    private void readItems(){
+        File filesDir=getFilesDir();
+
+        File file=new File(filesDir,"todo.txt");
+        try{
+            todoItems=new ArrayList<String>(FileUtils.readLines(file));
+        }catch (IOException e){
+
+        }
+    }
+
+    private void writeItems(){
+        File filesDir=getFilesDir();
+        File file=new File(filesDir,"todo.txt");
+        try{
+            FileUtils.writeLines(file,todoItems);
+        }catch (IOException e){
+
+        }
     }
 }
